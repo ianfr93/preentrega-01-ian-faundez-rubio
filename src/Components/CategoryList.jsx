752df00db9c73f61ab2ea436efd192119e1165ac
ianfr93 <albitox10@gmail.com>
2024-04-15 18:26:06 -0400

    navegacion

--- src/Components/CategoryList.jsx
+++ src/Components/CategoryList.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function categoryList() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <ul>
+
+<li>
+  <a href= "/#">Marcas</a>
+</li>
+
+<li>
+  <a href= "/#">Gamer Zone </a>
+</li>
+<li>
+  <a href= "/#">Arma tu pc</a>
+</li>
+<li>
+  <a href= "/#">Outlet Tecnologico</a>
+</li>
+<li>
+  <a href= "/#">Tiendas</a>
+</li>
+<li>
+  <a href= "/#">Servicios</a>
+</li>
+
+   </ul>
+  );
 }
 
-export default App
