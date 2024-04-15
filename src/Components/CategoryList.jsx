875117fd21979c66656be5b1a1cd8e0c05cbf7cb
ianfr93 <albitox10@gmail.com>
2024-04-15 19:35:43 -0400

    categorias

--- src/Components/CategoryList.jsx
+++ src/Components/CategoryList.jsx
@@ -1,28 +1,30 @@
-function categoryList() {
+import React from 'react';
+
+function CategoryList() {
   return (
-   <ul>
-
-<li>
-  <a href= "/#">Marcas</a>
-</li>
-
-<li>
-  <a href= "/#">Gamer Zone </a>
-</li>
-<li>
-  <a href= "/#">Arma tu pc</a>
-</li>
-<li>
-  <a href= "/#">Outlet Tecnologico</a>
-</li>
-<li>
-  <a href= "/#">Tiendas</a>
-</li>
-<li>
-  <a href= "/#">Servicios</a>
-</li>
-
-   </ul>
+    <ul>
+      <li>
+        <a href="/#">Marcas</a>
+      </li>
+      <li>
+        <a href="/#">Gamer Zone</a>
+      </li>
+      <li>
+        <a href="/#">Arma tu PC</a>
+      </li>
+      <li>
+        <a href="/#">Outlet Tecnológico</a>
+      </li>
+      <li>
+        <a href="/#">Tiendas</a>
+      </li>
+      <li>
+        <a href="/#">Servicios</a>
+      </li>
+    </ul>
   );
 }
+
+export default CategoryList;
+
 
