cc7d5783ad86c64aa0df5ef948ff8234b6097f8a
ianfr93 <albitox10@gmail.com>
2024-04-15 20:13:55 -0400

    icon

--- src/Components/CartIcon.jsx
+++ src/Components/CartIcon.jsx
@@ -1,8 +1,8 @@
-function CartIcon() {
+function CartIcon( {width, height}) {
   return (
     <svg
-      width="800px"
-      height="800px"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- src/Components/CartWidget.jsx
+++ src/Components/CartWidget.jsx
@@ -3,7 +3,7 @@ import CartIcon from "./CartIcon";
 function CartWidget() {
   return (
     <div>
-      <CartIcon/> 
+      <CartIcon width={40} height={40}/>
       <span>5</span>
     </div>
   );
