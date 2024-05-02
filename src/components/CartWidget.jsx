dac5cbf1436ee49b3de6f80a7f2b269eb3ad58c7
ianfr93 <albitox10@gmail.com>
2024-05-02 01:11:44 -0400

    cambio de rutas

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,16 +1,16 @@
-import CartIcon from "./CartIcon";
-import { useState } from "react";
-import "./CartIcon.css";
+import CartIcon from "./components/CartIcon/CartIcon"; 
+import { useState } from "react"; 
+import "./components/CartIcon/CartIcon.css"; 
 
 function CartWidget() {
-  const [CartItems, setCartItems]=
-  useState([5]);
+  const [CartItems, setCartItems] = useState([5]);
+  
   return (
     <div className="cart--container"> {/* Contenedor principal del carrito */}
-      <CartIcon width={30} height={30}/>
-      <span>{CartItems}</span>
+      <CartIcon width={30} height={30}/> 
+      <span>{CartItems}</span> 
     </div>
   );
 }
-export default CartWidget;
 
+export default CartWidget; 
