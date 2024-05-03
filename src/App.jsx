d4fc6951dd06f0705f91b2e234db1f6883b10d88
ianfr93 <albitox10@gmail.com>
2024-05-02 22:56:44 -0400

    rutas cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import "./App.css";
-import Brand from "./Components/Brand";
-import NavBar from "./Components/NavBar";
-import ItemListContainer from "./Components/ItemListContainer";
+import React from 'react';
+import './App.css'; 
+import Brand from './components/Brand';
+import NavBar from './components/NavBar/NavBar'; 
+import ItemListContainer from './components/ItemListContainer'; 
 
 function App() {
   return (
-    <div>
-      <div className="header__wrapper">
-        <Brand />
-        <NavBar />
-      </div>
-      <ItemListContainer Saludo="Hola Bienvenido a tu centro de ventas Tecnologico"/>
+    <div className="App">
+      <Brand />
+      <NavBar />
+      <ItemListContainer />
     </div>
   );
 }
