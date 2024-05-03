719d88de8aee01261022c122df18eee6b3c0d2c2
ianfr93 <albitox10@gmail.com>
2024-05-03 00:32:52 -0400

    se mueve cartwidget a carpeta

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import CategoryList from '../CategoryList/CategoryList'; 
 import CategoryListMobile from '../CategoryListMobile/CategoryListMobile'; 
 
-import CartWidget from "../CartWidget/CartWidget";
+import CartWidget from "./CartWidget/CartWidget";
 
 import './NavBar.css';
 import CartIcon from '../CartIcon';
