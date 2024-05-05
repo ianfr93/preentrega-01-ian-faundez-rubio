82fabccabd8e3ba4d443bf1b06d9f1b2b8c02634
ianfr93 <albitox10@gmail.com>
2024-05-05 19:33:07 -0400

    contador

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -17,7 +17,11 @@ export default function ItemCount({ stock, count, increment, decrement }) {
   return (
     <div className="item--count__container">
       <div className="item--count--counter__container">
-        <button className="item--count-button" onClick={handleDecrement}>
+        <button
+          className="item--count-button"
+          onClick={handleDecrement}
+          disabled={count === 0} // Desactivar cuando count es 0
+        >
           -
         </button>
         <span className="item--count-button">{count}</span>
