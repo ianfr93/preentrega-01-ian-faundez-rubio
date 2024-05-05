51fd205c468de4e5372c094072e4c4fa585d7650
ianfr93 <albitox10@gmail.com>
2024-05-05 17:27:07 -0400

    stock

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -11,7 +11,7 @@ function ItemListContainer({ saludo }) {
         count={count}
         decrement={decrement}
         increment={increment}
-        stock={3}
+        stock={5}
       />
     </div>
   );
