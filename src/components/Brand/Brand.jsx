68210cea228b041f49c9972a3e5c3af94a11b072
ianfr93 <albitox10@gmail.com>
2024-05-05 02:32:18 -0400

    nuevos cambios

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,11 +1,10 @@
-// Brand.jsx
 import React from 'react';
-import logo from '../../assets/azul.svg';
+import logo from '../../assets/blanco.svg';
 import './Brand.css'; 
 
 function Brand() {
   return (
-    <div>
+    <div className="brand-container"> 
       <a href="/">
         <img src={logo} alt="Logo de Shoppy" className="brand-logo" />
       </a>
