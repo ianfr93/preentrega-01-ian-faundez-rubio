0dfbe7ceea81cad284fa2aa1762c94d2826e720f
ianfr93 <albitox10@gmail.com>
2024-05-05 17:37:49 -0400

    cambios

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,16 +1,29 @@
+import React from "react";
 import "./ItemCount.css";
 
 export default function ItemCount({ stock, count, increment, decrement }) {
+  const handleDecrement = () => {
+    if (count > 0) {
+      decrement();
+    }
+  };
+
+  const handleIncrement = () => {
+    if (count < stock) {
+      increment();
+    }
+  };
+
   return (
     <div className="item--count__container">
       <div className="item--count--counter__container">
-        <button className="item--count-button" onClick={decrement}>
+        <button className="item--count-button" onClick={handleDecrement}>
           -
         </button>
         <span className="item--count-button">{count}</span>
         <button
           className="item--count-button"
-          onClick={increment}
+          onClick={handleIncrement}
           disabled={count >= stock}
         >
           +
