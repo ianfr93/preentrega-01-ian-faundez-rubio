4bc1a7149b799a9d162fe9c2264b327acb310671
ianfr93 <albitox10@gmail.com>
2024-05-13 22:48:27 -0400

    categorias

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import ItemList from '../ItemList/ItemList';
 
 function ItemListContainer({ saludo, products }) {
